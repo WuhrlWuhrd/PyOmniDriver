33919781700ead75a3ed1fb041f099c040d8d3fd
William Wood <waw31@cam.ac.uk>
2024-02-19 11:59:41 +0000

    Updates

--- pyomnidriver/__init__.py
+++ pyomnidriver/__init__.py
@@ -3,10 +3,17 @@ import os
 import jpype
 import jpype.imports
 import urllib.request
-import atexit
 from jpype import JProxy
 
 path = os.path.dirname(os.path.realpath(__file__))
+
+if not os.path.exists(os.path.join(path, "OO.jar")):
+    raise Exception("OO.jar not found! Try reinstalling pyomnidriver!")
+
+# Link in JISA.jar classes
+jpype.addClassPath(os.path.join(path, "OO.jar"))
+jpype.imports.registerDomain("com.oceanoptics")
+
 
 def load(jvmPath=None):
 
@@ -14,9 +21,6 @@ def load(jvmPath=None):
         jpype.shutdownJVM()
 
     if not jpype.isJVMStarted():
-
-        if not os.path.exists(os.path.join(path, "OO.jar")):
-            raise Exception("OO.jar not found! Try reinstalling pyomnidriver!")
 
         complete = ""
 
@@ -34,18 +38,6 @@ def load(jvmPath=None):
             elif os.path.exists(mac):
                 complete = mac
 
-        # Link in JISA.jar classes
-        jpype.addClassPath(os.path.join(path, "OO.jar"))
-        jpype.imports.registerDomain("com.oceanoptics")
-
         # Start the JVM
         jpype.startJVM(jvmpath=complete, convertStrings=True)
-
-        atexit.register(shutdown)
-
-
-def shutdown():
-    
-    if jpype.isJVMStarted():
-        jpype.shutdownJVM()
 
